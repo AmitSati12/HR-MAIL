dc64ac3e15b63c2627d2d58369199b319a07dc9e
Amit <satiamit01@gmaili.com>
2026-03-16 23:09:23 +0530

    Db changes

--- backend/db.py
+++ backend/db.py
@@ -1,24 +1,49 @@
-import oracledb
+from pymongo import MongoClient
+import os
 
-# Oracle connection details
-connection = oracledb.connect(
-    user="truckuser",
-    password="truckuser",
-    dsn="localhost:1521/TRUCKLDB_PDB"
-)
+# Read MongoDB URI from environment variable
+MONGO_URI = os.getenv("MONGO_URI")
+
+client = MongoClient(MONGO_URI)
+
+# database name
+db = client["jobmailer"]
+
+# collection name
+collection = db["applications"]
 
 def insert_application(company, role, email, message):
 
-    cursor = connection.cursor()
+    data = {
+        "company": company,
+        "role": role,
+        "email": email,
+        "message": message
+    }
 
-    query = """
-    INSERT INTO JOB_APPLICATIONS
-    (COMPANY_NAME, JOB_ROLE, HR_EMAIL, MESSAGE)
-    VALUES (:1, :2, :3, :4)
-    """
+    collection.insert_one(data)
 
-    cursor.execute(query, (company, role, email, message))
+# import oracledb
 
-    connection.commit()
+# # Oracle connection details
+# connection = oracledb.connect(
+#     user="truckuser",
+#     password="truckuser",
+#     dsn="localhost:1521/TRUCKLDB_PDB"
+# )
 
-    cursor.close()
+# def insert_application(company, role, email, message):
+
+#     cursor = connection.cursor()
+
+#     query = """
+#     INSERT INTO JOB_APPLICATIONS
+#     (COMPANY_NAME, JOB_ROLE, HR_EMAIL, MESSAGE)
+#     VALUES (:1, :2, :3, :4)
+#     """
+
+#     cursor.execute(query, (company, role, email, message))
+
+#     connection.commit()
+
+#     cursor.close()
