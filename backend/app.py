5ddcdd27e184b3c914dea6988a3262699656afe8
Amit <satiamit01@gmaili.com>
2026-03-16 23:15:15 +0530

    Db changes 2 app.py

--- backend/app.py
+++ backend/app.py
@@ -10,6 +10,9 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+@app.route("/")
+def health():
+    return "Backend running successfully"
 
 
 @app.route("/bulk_send", methods=["POST"])
